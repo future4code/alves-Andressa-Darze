c1722127ffbf0989290816933d2ec57e850eacad
Andressa Urbano de Sousa Darze <andressa.darze@gmail.com>
2022-07-13 19:22:43 -0300

    Renderizei lista de trips na AdminHomePage

--- modulo3/projeto-labex/labex/src/pages/AdminHomePage.jsx
+++ modulo3/projeto-labex/labex/src/pages/AdminHomePage.jsx
@@ -1,13 +1,57 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import axios from 'axios'
 import { goBack, goToCreateTripPage, goToTripDetailsPage, goToLoginPage } from '../routes/Coordinator'
 import { useProtectedPage } from '../constants/constants'
+import { BASE_URL } from '../constants/constants'
+import styled from 'styled-components'
+
+const CardTrip = styled.div`
+  border: 1px solid black;
+  padding: 10px;
+  margin: 10px;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  /* justify-content: space-between; */
+`
+
 
 export const AdminHomePage = () => {
   
   const navigate = useNavigate()
 
   useProtectedPage()
+
+  const [listTrips, setListTrips] = useState([])
+
+  useEffect(() => {
+    getTrips()
+  }, [])
+
+  const getTrips = () => { 
+    axios.get(`${BASE_URL}/trips`)
+    .then((res) => {
+        setListTrips(res.data.trips)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+  }
+
+  const newListTrips = listTrips.map((trip) => {
+    return (
+      <CardTrip key={trip.id}>
+        <h3>{trip.name}</h3>
+        <h4>{trip.description}</h4>
+        <h4>Planeta: {trip.planet}</h4>
+        <h4>Duração: {trip.durationInDays} dias</h4>
+        <h4>Data: {trip.date}</h4>
+        <button onClick={() => goToTripDetailsPage(navigate)}>Detalhes</button>
+      </CardTrip>
+    )
+  })
+
 
   const useLogOut = () => {
     localStorage.removeItem("token")
@@ -21,6 +65,7 @@ export const AdminHomePage = () => {
       <button onClick={() => goToCreateTripPage(navigate)}>Criar viagem</button>
       <button onClick={useLogOut}>Logout</button>
       <button onClick={() => goToTripDetailsPage(navigate)}>Detalhes de uma viagem</button>
+      {newListTrips}
     </div>
   )
 }
--- modulo3/projeto-labex/labex/src/pages/TripDetailsPage.jsx
+++ modulo3/projeto-labex/labex/src/pages/TripDetailsPage.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { goBack } from '../routes/Coordinator'
 import { useProtectedPage } from '../constants/constants'
+import { axios } from 'axios'
+import { BASE_URL } from '../constants/constants'
 
 export const TripDetailsPage = () => {
 
   const navigate = useNavigate()
 
   useProtectedPage()
+
+  const getTripDetails = (id) => {
+    axios.get(`${BASE_URL}/trip/${id}`)
+    .then((res) => {
+        console.log(res)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+  }
 
   return (
     <div>
