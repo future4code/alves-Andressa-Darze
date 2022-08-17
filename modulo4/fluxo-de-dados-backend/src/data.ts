5e826533f8c521dfc74eea54a98a5dd1af2ad082
Andressa Urbano de Sousa Darze <andressa.darze@gmail.com>
2022-08-17 18:18:11 -0300

    Exercício Fluxo de Dados Backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,30 @@
+// Exercício 2 
+
+export type Product = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export const products : Product[] = [
+    {
+        id: "1",
+        name: "caderno",
+        price: 49.50
+    },
+    {
+        id: "2",
+        name: "caneta",
+        price: 3.70
+    },
+    {
+        id: "3",
+        name: "borracha",
+        price: 2
+    },
+    {
+        id: "4",
+        name: "planner",
+        price: 89.90
+    }
+]
